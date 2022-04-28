4182110ac6f0ac02abc9db3d58a48e1ce3ebab99
Aditya <akaditya394@gmail.com>
2022-04-28 16:17:50 +0530

    content added to header file

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import styles from "./Header.module.css";
 
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <div className={styles.main}>
+      <div>
+        <h1 className={styles.logo}>getShows</h1>
+      </div>
+      <div className={styles.navlink}>Discover</div>
+      <div className={styles.navlink}>About</div>
+      <div className={styles.navlink}>Contact Us</div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
