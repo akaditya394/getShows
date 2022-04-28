3f62e0cc58d260a155d6868af413018547a6e7c9
Aditya <akaditya394@gmail.com>
2022-04-28 19:25:28 +0530

    functionality figured out

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
    <Routes>
      <Route path="/" element={<Home />}/>
-     <Route path="/details/:name" element={<Details />}/>
+     <Route path="/details/:position" element={<Details />}/>
    </Routes>
   )
 }
--- src/pages/Details/Detail.jsx
+++ src/pages/Details/Detail.jsx
@@ -1,12 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect, useCallback } from "react";
+import { useParams } from "react-router-dom";
 import Header from "../../components/Header/Header";
 
 function Detail() {
+  const [list, setList] = useState([]);
+
+  const fetchShows = useCallback(async () => {
+    try {
+      const response = await fetch(
+        "https://api.tvmaze.com/search/shows?q=all#"
+      );
+      const data = await response.json();
+      setList(data);
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchShows();
+  }, [fetchShows]);
+
+  const { position } = useParams();
+  console.log(position);
+
   return (
     <div>
-        <Header />
+      <Header />
+      <p>{list[position].show.name}</p>
     </div>
-  )
+  );
 }
 
-export default Detail
+export default Detail;
